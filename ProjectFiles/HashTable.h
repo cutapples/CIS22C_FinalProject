c15b391c980d792c9496f950975e6391ccffc246
Peter <petershin731@gmail.com>
2017-03-14 12:40:53 -0700

    Update HashTable.h

--- ProjectFiles/HashTable.h
+++ ProjectFiles/HashTable.h
@@ -1,30 +1,26 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
-#include <string>
-#include "Node.h"
 
-class HashTable
-{
+#include "HashNode.h"
+#include "SmashHero.h"
+
+class HashTable {
 private:
-	static const int tableSize = 17;
-
-	struct item{
-		int keyValue, rarity;
-		std::string name;
-		item* next;
-	};
-	
-	item *hTable[tableSize];
+	const int tableSize = 257;
+	HashNode** hTable, **top;
+	int collisions;
 
 public:
 	HashTable();
-	int Hash(std::string key);
-	void AddItem(int key, std::string name, int rarity);
-	int numberOfItem(int index);
-	void printTable();
-	void printIndexItem(int index);
-	void searchByName(std::string namee);
-	void removeItem(std::string namee);
+	~HashTable();
+	int Hash(int key);
+	int getCollisions();
+	void getItem(int key);
+	void addItem(SmashHero* data, int key);
+	void removeItem(int key);
+	
 };
 
-#endif
+
+
+#endif HASHTABLE_H
