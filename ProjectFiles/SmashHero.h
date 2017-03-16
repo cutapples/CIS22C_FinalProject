b694996b0a6d5906398d93529950c010de37ab52
James May <may.james.m@gmail.com>
2017-03-15 17:59:16 -0700

    Update SmashHero.h

--- ProjectFiles/SmashHero.h
+++ ProjectFiles/SmashHero.h
@@ -6,51 +6,66 @@ using namespace std;
 
 class SmashHero {
 private:
-    double primaryKey;
-    string heroName;
-    int rarityIndex;
-    //stats
-    
+	int primaryKey;
+	string heroName;
+	int rarityIndex;
+	//stats
+
 public:
-    SmashHero(){};
-    SmashHero(string line){
-        //The tabs are being found from the input line that we ghet from the file.
-        int tab1 = line.find("\t");
-        int tab2 = line.find("\t", tab1 + 1);
-        int tab3 = line.find("\t", tab2 + 1);
-        
-        //The string is being parsed into seperate variables.
-        string primaryKey = line.substr(0, tab1);
-        string heroName = line.substr(tab1 + 1, tab2 - tab1 - 1);
-        string rarity = line.substr(tab2 + 1, tab2 - tab3 - 1);
-        
-        //The functions are beijng called and each SmashHero attribute is being set with the variable being passed in.
-        SmashHero::setPrimaryKey(stod(primaryKey));
-        SmashHero::setHeroName(heroName);
-        SmashHero::setRarity(stoi(rarity));
-    };
-    
-    void setPrimaryKey(double primaryKey) { this->primaryKey = primaryKey; };
-    void setHeroName(string heroName) { this->heroName = heroName; };
-    void setRarity(int rarityIndex){ this->rarityIndex = rarityIndex; };
-    friend ostream& operator<<(ostream& os, SmashHero& hero){
-        
-        //Case being used if the Heroes are being output to the screen.
-        if(&os == &cout){
-            
-            
-        }
-        
-        // Case being used if the heroes are going to be saved into a file
-        os << hero.primaryKey << "\t";
-        os << hero.heroName << "\t";
-        os << hero.rarityIndex << "\t";
-        
-        
-        return os;
-    }
-    
-    
+	SmashHero() {};
+	SmashHero(string line) {
+		//The tabs are being found from the input line that we ghet from the file.
+		int tab1 = line.find("\t");
+		int tab2 = line.find("\t", tab1 + 1);
+		int tab3 = line.find("\t", tab2 + 1);
+
+		//The string is being parsed into seperate variables.
+		string primaryKey = line.substr(0, tab1);
+		string heroName = line.substr(tab1 + 1, tab2 - tab1 - 1);
+		string rarity = line.substr(tab2 + 1, tab2 - tab3 - 1);
+
+		//The functions are beijng called and each SmashHero attribute is being set with the variable being passed in.
+		SmashHero::setPrimaryKey(stoi(primaryKey));
+		SmashHero::setHeroName(heroName);
+		SmashHero::setRarity(stoi(rarity));
+	};
+
+	int getPrimaryKey() { return this->primaryKey; };
+	string getHeroName() { return this->heroName; };
+	int getRarityIndex() { return this->rarityIndex; };
+
+	void setPrimaryKey(int primaryKey) { this->primaryKey = primaryKey; };
+	void setHeroName(string heroName) { this->heroName = heroName; };
+	void setRarity(int rarityIndex) { this->rarityIndex = rarityIndex; };
+	friend ostream& operator<<(ostream& os, SmashHero& hero) {
+
+		//Case being used if the Heroes are being output to the screen.
+
+		// Case being used if the heroes are going to be saved into a file
+		os << hero.primaryKey << "\t";
+		os << hero.heroName << "\t";
+		os << hero.rarityIndex << "\t";
+
+
+		return os;
+	}
+
+	friend bool operator>(SmashHero hero, int userKey) {
+		return (hero.getPrimaryKey() > userKey);
+	}
+
+	friend bool operator<(SmashHero hero, int userKey) {
+		return(hero.getPrimaryKey() < userKey);
+	}
+
+	friend bool operator==(SmashHero hero, int userKey) {
+		return(hero.getPrimaryKey() == userKey);
+	}
+
+	friend bool operator!=(SmashHero hero, int userKey) {
+		return (hero.getPrimaryKey() != userKey);
+	}
+
 };
 
 #endif SMASHHERO_H
